d8c4bed444db2b55edf6c842a94d115736e4e9e2
yoshiko-u <62194599+yoshiko-u@users.noreply.github.com>
2020-04-17 09:35:44 +0900

    master

---
+++ .py/rensu_01.py
@@ -0,0 +1,84 @@
+
+msg = "Hello World"
+print("msg")
+
+
+
+#2020/4/3-
+s = "he\nllo wor\tld"
+html ="""<html>
+<body></body>
+</html>"""
+print(s)
+print(html)
+
+pi = 3.141592
+diameter = 12756.274
+print( pi*diameter ) 
+
+cal_per_1kg = 7200
+cal_per_1minjog = 7.76
+min_to_lose1kg = cal_per_1kg / cal_per_1minjog
+
+hours_to_lose1kg = min_to_lose1kg / 60
+print(hours_to_lose1kg)
+
+day = 24
+str_day =str(day)
+date = str_day+"日"
+print(date)
+
+int("200")
+float("3.14159265358979")
+
+monk_fish_team = [158, 157, 163, 157, 145]
+
+total = sum(monk_fish_team)
+length = len(monk_fish_team)
+mean = total/length
+variance = 0
+
+for height in monk_fish_team:
+    variance += (height-mean)**2
+
+variance = variance/length
+print(variance)
+
+print(variance**0.5)
+
+volleyball_team = [143, 167, 170, 165]
+
+total2 =sum(volleyball_team)
+length2 =len(volleyball_team)
+mean2 = total2/length2
+variance2 = 0
+
+for height in volleyball_team:
+    variance2 += (height-mean2)**2
+
+variance2 = variance2/length2
+print(variance2**0.5)
+
+for cnt in range(10):
+    print(cnt)
+
+savings = 100
+for i in range(15):
+    savings += savings*0.05
+print(savings)
+
+if 1 ==1:
+    print("1番目はtrue")
+if 5^(4-4)+9 ==10:
+    print("2番目はtrue")
+if 2< len([0, 1, 2]):
+    print("3番目はtrue")
+if sum([1, 2, 3, 4])<10:
+    print("4番目はtrue")
+
+    a_year = 2080
+    if a_year >= 1993:
+        if a_year ==1993:
+            print(a_year,"年、れに誕生")
+        else:
+            print(a_year,"年、れに",a_year-1993,"歳")
