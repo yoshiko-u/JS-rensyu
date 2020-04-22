e6fe30f4e2fea03f59a0f34ff5527d6b26c1ab96
yoshiko-u <62194599+yoshiko-u@users.noreply.github.com>
2020-04-22 18:25:44 +0900

    setを使う

--- .py/rensu_01.py
+++ .py/rensu_01.py
@@ -82,3 +82,52 @@ if sum([1, 2, 3, 4])<10:
             print(a_year,"年、れに誕生")
         else:
             print(a_year,"年、れに",a_year-1993,"歳")
+
+
+purple = {"ニックネーム":"れにちゃん",
+          "出身地":"神奈川県",
+          "キャッチフレーズ":"感電少女"}
+print(purple["出身地"])
+print(purple)
+
+purple["キャッチフレーズ"] = "鋼少女"
+print(purple)
+purple["生年月日"] = "1993年6月21日"
+print(purple)
+del purple["ニックネーム"]
+print(purple)
+def convert_number(num):
+    #アラビア数字とローマ数字の対応表をディクショナリに定義
+    roman_nums = {1:"Ⅰ",2:"Ⅱ",3:"Ⅲ",4:"Ⅳ",5:"Ⅴ",
+                  6:"Ⅵ",7:"Ⅶ",8:"Ⅷ",9:"Ⅸ" }
+    #ディクショナリのキーとして引数の整数が存在していたら
+    #キーに対応する値を戻り値にする
+    if num in roman_nums:
+        return roman_nums[num]
+    else:
+        return "[変更できません]"
+
+prime = {2,3,5,7,13,17}
+fib = {1,1,2,3,5,8,13}
+
+prime_fib = prime | fib
+print(prime_fib)
+
+dice = {1,2,3,4,5,6}
+even = {2,4,6,8,10}
+
+odd_dice = dice - even
+print(odd_dice)
+
+prefs = {"北海道","青森","秋田","岩手"}
+capitals = {"札幌","青森","秋田","盛岡"}
+
+pref_cap = prefs & capitals
+print(pref_cap)
+
+pref_cap2 = prefs^capitals
+print(pref_cap2)
+
+codon = ["ATG","GGC","TCC","AAG","TTC","TGG","GAC","TCC"]
+s_codon = set(codon)
+print(len(codon),len(s_codon))
